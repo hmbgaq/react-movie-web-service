871f11eb8081bf492acdbf2028971721ccda712b
hmbgaq1 <hmbgaq1@gmail.com>
2022-07-12 22:55:55 +0900

    work detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,7 +1,10 @@
-import { useEffect } from "react";
-import {useParams} from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Movie from "../components/Movie";
 
 function Detail() {
+    const [loading, setLoading] = useState(true);
+    const [movie, setMovie] = useState([]);
     const { id } = useParams();
 
     const getMovies = async () => {
@@ -9,12 +12,26 @@ function Detail() {
             await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
         ).json();
         console.log(json);
+        setMovie(json.data.movie)
+        setLoading(false);
     }
 
     useEffect(() => {
         getMovies();
     }, [])
-    return <h1>Detail</h1>
+    return <div>
+        {loading ? <h1>Loading...</h1> : 
+        <div>
+            <Movie 
+                id = {movie.id}
+                coverImg={movie.background_image}
+                title = {movie.title}
+                summary = {movie.description_full}
+                genres = {movie.genres}
+            />
+        </div>
+        }
+    </div>
 }
 
 export default Detail;
